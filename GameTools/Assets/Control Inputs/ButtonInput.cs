72a7f794cfc0bcfa7d559513444837901d7c5776
Kyler Alford <alfordkyler@gmail.com>
2020-05-22 15:31:11 -0400

    ButtonInput ScriptableObject
    Set up the ButtonInput scriptable object to be driven by the ButtonInputDriver component.

---
+++ GameTools/Assets/Control Inputs/ButtonInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BoolVariable", menuName = "Inputs/Button", order = 51)]
+public class ButtonInput : ScriptableObject
+{
+    [Tooltip("The keystroke or joystick button input for this InputButton.")]
+    public string keystroke;
+    [Tooltip("An alternate keystroke or joystick button input for this InputButton, typically for cross platform input.")]
+    public string keystrokeAlt;
+
+    [HideInInspector]
+    public bool isButtonPressed = false;
+    [HideInInspector]
+	public bool onButtonDown = false;
+    [HideInInspector]
+	public bool onButtonTimer = false;
+    [HideInInspector]
+	public bool onButtonUp = false;
+}
---
+++ GameTools/Assets/Scriptable Object Save Load/ScriptableObjectSaveLoad.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class ScriptableObjectSaveLoad : MonoBehaviour
+{
+
+    [Tooltip("Leaving this field as 'null' will save in the root of the persistent data path.")]
+    [SerializeField] private string saveFolder = null;
+    [SerializeField] private bool loadOnAwake = false;
+    [SerializeField] private bool loadOnEnable = false;
+    [SerializeField] private bool loadOnStart = false;
+    [SerializeField] private bool saveOnDisable = false;
+    [Tooltip("All scriptable objects will save as the name of the scriptable object.")]
+    [SerializeField] private ScriptableObject[] saveObjects = new ScriptableObject[1];
+
+    private void Awake()
+    {
+        if (loadOnAwake)
+        {
+            Load();
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (loadOnEnable)
+        {
+            Load();
+        }
+    }
+
+    private void Start()
+    {
+        if (loadOnStart)
+        {
+            Load();
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (saveOnDisable)
+        {
+            Save();
+        }
+    }
+
+    public void Save()
+    {
+        // Create save data directory if none exist
+        if (!Directory.Exists(Application.persistentDataPath + "/" + saveFolder))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/" + saveFolder);
+        }
+
+        // Saves each array entry
+        for (int i = 0; i < saveObjects.Length; i++)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + "/" + saveFolder + "/" + saveObjects[i].name + ".json");
+            string json = JsonUtility.ToJson(saveObjects[i]);
+            // May want to put a layer of generic encryption here
+            bf.Serialize(file, json);
+            file.Close();
+        }
+    }
+
+    public void Load()
+    {
+        // Create a save data directory if none exist
+        if (!Directory.Exists(Application.persistentDataPath + "/" + saveFolder))
+        {
+            Directory.CreateDirectory(Application.persistentDataPath + "/" + saveFolder);
+        }
+
+        // Load each array entry
+        for (int i = 0; i < saveObjects.Length; i++)
+        {
+            // Make sure save file exists
+            if (File.Exists(Application.persistentDataPath + "/" + saveFolder + "/" + saveObjects[i].name + ".json"))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + "/" + saveFolder + "/" + saveObjects[i].name + ".json", FileMode.Open);
+                // May want to put a layer of generic decryption here
+                JsonUtility.FromJsonOverwrite((string)bf.Deserialize(file), saveObjects[i]);
+                file.Close();
+            }
+        }
+    }
+
+}
