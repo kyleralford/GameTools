1f053a83699ed6af160710959003d72d724dc348
Kyler Alford <alfordkyler@gmail.com>
2020-07-05 14:51:26 -0400

    Added Button Input Driver for UI Buttons
    The generic input driver and UI input driver currently override each other. Need to fix.

---
+++ GameTools/Assets/Control Inputs/ButtonInputDebug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInputDebug : MonoBehaviour
+{
+    public ButtonInput testInput;
+    public int cycles = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (testInput.onButtonDown)
+        {
+            cycles += 1;
+            Debug.Log("Button has been pressed down " + cycles + " times.");
+        }
+    }
+}
---
+++ GameTools/Assets/Debug Scripts/ForceFramerate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceFramerate : MonoBehaviour
+{
+    public int targetFramerate = 30;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = targetFramerate;
+    }
+}
