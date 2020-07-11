49281a802b14132843bf91c12e29eb515dda0437
Kyler Alford <alfordkyler@gmail.com>
2020-07-11 19:30:06 -0400

    Started RuntimeSet Implementation
    No idea what I'm doing. Need to research.

---
+++ GameTools/Assets/Runtime Sets/AddGameObjectToRuntimeSet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddGameObjectToRuntimeSet : MonoBehaviour
+{
+    public RuntimeSet<GameObject> runtimeSets;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
